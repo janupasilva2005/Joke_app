a376e79b4e64c96edb130e8a25bf3e148366cb96
janupasilva2005 <janupasilva2005@gmail.com>
2023-03-02 10:19:47 +0530

    Finished a basic version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +13,9 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Joke app',
       home: Body(),
+      title: 'Joke app',
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -27,12 +29,66 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  late Future<String> joke;
+
+  // Fetching the joke API
+  Future<String> fetchAPI() async {
+    var data = await Dio().get('https://api.chucknorris.io/jokes/random');
+
+    // returning the value
+    return data.data['value'];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Setting the joke value
+    joke = fetchAPI();
+  }
+
+  // Refreshing the joke
+  void refreshJoke() {
+    setState(() {
+      joke = fetchAPI();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        color: const Color(0xff1d2136),
+        child: Center(
+          child: FutureBuilder(
+            future: joke,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+
+              return Container(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  '${snapshot.data}',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: const Text('Joke app'),
+        backgroundColor: const Color(0xff1d2136),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: refreshJoke,
+        backgroundColor: const Color(0xff323244),
+        child: const Icon(Icons.refresh),
       ),
     );
   }
